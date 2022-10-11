bcdd0e637bac902d8a5ec934f64f5f80a5369d82
Ayomitan Abraham Batimehin <abatimehin@gmail.com>
2022-10-11 23:23:05 +0100

    Firebase Installation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-
+import * as firebase from "firebase";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -14,6 +14,16 @@ import { Navigation } from "./src/infrastructure/navigation";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbf8ydm3stNd6jJvXo7CfQJ88EU9tTv64",
+  authDomain: "foodproj-faa2e.firebaseapp.com",
+  projectId: "foodproj-faa2e",
+  storageBucket: "foodproj-faa2e.appspot.com",
+  messagingSenderId: "31645343536",
+  appId: "1:31645343536:web:8f4c0dc5e3755cf66372f5",
+};
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
